e92069977459d3d98e8ff691d07308a730a33e95
Mark Tse <mark.tse@neverendingqs.com>
2019-10-13 21:12:53 -0400

    Vue CLI and Fastify init.

---
+++ server/services/root.js
@@ -0,0 +1,21 @@
+'use strict'
+
+module.exports = function (fastify, opts, next) {
+  fastify.get('/', function (request, reply) {
+    reply.send({ root: true })
+  })
+
+  fastify.get('/test', function (request, reply) {
+    reply.send({ test: true })
+  })
+
+  next()
+}
+
+// If you prefer async/await, use the following
+//
+// module.exports = async function (fastify, opts) {
+//   fastify.get('/', async function (request, reply) {
+//     return { root: true }
+//   })
+// }
